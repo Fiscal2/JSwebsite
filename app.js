493ed5945bf7b0253f541d826b22650870fae6b2
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-05-18 21:31:59 -0400

    pickle morty

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const button = document.querySelector('.btn');
+const image = document.querySelector('img')
+
+const pickleRick = "https://static.wikia.nocookie.net/be-like-bro/images/7/7b/Picklerick.jpg/revision/latest/top-crop/width/360/height/450?cb=20171118185327"
+const mortySmith = "https://static.tvtropes.org/pmwiki/pub/images/morty_smith_2.png"
 
 button.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
@@ -11,6 +15,14 @@ button.addEventListener('click', function() {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
+    }
+
+    const imageSRC = image.getAttribute('src')
+
+    if(imageSRC === pickleRick){
+        image.setAttribute('src', mortySmith)
+    } else{
+        image.setAttribute('src', pickleRick)
     }
 
     console.log('current class name: ' + className);
