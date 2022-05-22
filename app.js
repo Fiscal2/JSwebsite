cafddc3b8c697d71b569de134430368eb365bb58
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-05-21 23:05:28 -0400

    pickle rickkkkkk

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const image = document.querySelector('img')
 const pickleRick = "https://static.wikia.nocookie.net/be-like-bro/images/7/7b/Picklerick.jpg/revision/latest/top-crop/width/360/height/450?cb=20171118185327"
 const mortySmith = "https://static.tvtropes.org/pmwiki/pub/images/morty_smith_2.png"
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
@@ -14,29 +14,63 @@ button.addEventListener('click', function() {
 
     const imageSRC = image.getAttribute('src')
 
-    if(className == "light-theme") {
+    if (className == "light-theme") {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
     }
 
-    if(imageSRC === pickleRick){
+    if (imageSRC === pickleRick) {
         image.setAttribute('src', mortySmith)
-    } else{
+    } else {
         image.setAttribute('src', pickleRick)
     }
 });
 
-function Pressed(){
+function Pressed() {
     const text = document.getElementById("inp").value.toLowerCase();
     const mortyText = "morty smith"
     const pickleRickText = "pickle rick"
 
-    if(text.length > 3 && mortyText.includes(text)){
+    if (text.length > 3 && mortyText.includes(text)) {
         image.setAttribute('src', mortySmith)
-    }else if(pickleRickText.includes(text)) {
+    } else if (pickleRickText.includes(text)) {
         image.setAttribute('src', pickleRick)
-    }else{
+    } else {
         console.log(text)
     }
 }
+
+const rickAndMortyUrl = "https://rickandmortyapi.com/api/character/1"
+const div = document.getElementById('rickAndMorty');
+const list = document.createDocumentFragment();
+
+fetch(rickAndMortyUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+
+        console.log(data)
+        console.log(data.name)
+        let name = document.createElement('h2');
+
+        let picture = document.createElement('img');
+        picture.setAttribute("height", 100);
+        picture.setAttribute("width", 100);
+
+        let status = document.createElement('span');
+
+        picture.src = `${data.image}`;
+        name.innerHTML = `Name: ${data.name}`;
+        status.innerHTML = `Status: ${data.status}`;
+
+        div.appendChild(picture);
+        div.appendChild(name);
+        div.appendChild(status);
+
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
