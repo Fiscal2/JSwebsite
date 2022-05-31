82db79a778d1770d0e9ad967b6f958ec286e65b9
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-05-30 22:56:53 -0400

    check it out

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ const rickAndMortyUrl = "https://rickandmortyapi.com/api/character/1"
 function switchTheme() {
     console.log(document.body.classList)
     const currentBackground = document.body.classList[0]
-    const text = document.getElementById("Title")
+    const title = document.getElementById("Title").classList[0]
     const themeButton = document.getElementById("themebutton")
 
     if (currentBackground == "bg-dark") {
         themeButton.innerHTML = "Dark"
         document.body.classList.replace(currentBackground, "bg-info")
-        document.body.classList.replace(text, "text-white")
+        document.body.classList.replace(title, "text-white")
     } else {
         themeButton.innerHTML = "Light"
         document.body.classList.replace(currentBackground, "bg-dark")
