82422759eba6131aa4db4747ea321a4076eb6e36
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-06-02 12:03:53 -0400

    kinda cool??

--- portal.js
+++ portal.js
@@ -1,36 +1,41 @@
 const locationBaseUrl = "https://rickandmortyapi.com/api/location/"
 
-function GetAllLocations(url) {
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data)
-            const cardRow = document.getElementById("cardrow");
+async function FetchAllLocations(url) {
+    const locationResponseData = await fetch(url);
 
-            for (let location of data.results) {
-                const columnSmall = document.createElement("div");
-                columnSmall.classList.add("col-sm-4")
-                const card = document.createElement("div");
-                card.classList.add("card", "my-2", "bg-transparent", "text-white");
-                card.innerHTML =
-                    `
-                    <h5 class="card-header bg-success">${location.name}</h5>
-                    <div class="card-body bg-success bg-opacity-75">
-                        <h5 class="card-title">${location.dimension}</h5>
-                        <h6 class="card-subtitle mb-2">Type: ${location.type}</h6>
-                        <p class="card-text">Number of residents: ${location.residents.length}</p>
-                    </div>
-                `
-                columnSmall.appendChild(card);
-                cardRow.appendChild(columnSmall);
-            }
-
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    if (locationResponseData.ok) {
+        const locationResponseJson = await locationResponseData.json();
+        return locationResponseJson
+    } else {
+        return alert(`HTTP-Error: ${locationResponseData.status}`);
+    }
 }
 
-GetAllLocations(locationBaseUrl)
+function LocationCardConstructor(locationPromise) {
+    locationPromise.then(locationData => {
+        const cardRow = document.getElementById("cardrow");
+
+        for (let location of locationData.results) {
+            const columnSmall = document.createElement("div");
+            columnSmall.classList.add("col-sm-4")
+            const card = document.createElement("div");
+            card.classList.add("card", "my-2", "bg-transparent", "text-white");
+            card.innerHTML =
+                `
+            <h5 class="card-header bg-success">${location.name}</h5>
+            <div class="card-body bg-success bg-opacity-75">
+                <h5 class="card-title">${location.dimension}</h5>
+                <h6 class="card-subtitle mb-2">Type: ${location.type}</h6>
+                <p class="card-text">Number of residents: ${location.residents.length}</p>
+            </div>
+        `
+            columnSmall.appendChild(card);
+            cardRow.appendChild(columnSmall);
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+
+}
+
+LocationCardConstructor(FetchAllLocations(locationBaseUrl));
