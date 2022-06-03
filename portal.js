63d7ddc6161f938ef48fdef7d0ffd4b2239ff593
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-06-03 10:21:50 -0400

    modal magic

--- portal.js
+++ portal.js
@@ -39,37 +39,62 @@ async function LocationCardConstructor() {
         const characterInfo = await ResidentsToCharacterObjects(locationBaseUrl, (location.id - 1));
         console.log(characterInfo);
 
-        const modal = document.createElement('div');
 
         let characterNameList = [];
 
         if (Array.isArray(characterInfo)) {
             for (character of characterInfo) {
                 characterNameList.push(character.name)
-
             }
         } else {
             characterNameList.push(characterInfo.name)
 
         }
 
-        modal.innerHTML =
-            `
-            <div class="modal fade" id="locationModal${location.id}" data-bs-keyboard="false" tabindex="-1"
-                aria-labelledby="locationModal" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">${location.name}</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body" id="modalbody">${characterNameList}</div>
-                    </div>
-                </div>
-            </div>
-        `
+        const modalContainerDiv = document.createElement('div');
+        modalContainerDiv.classList.add("modal", "fade")
+        const modalContainer = setElementAttributes(modalContainerDiv,
+            {
+                "id": `locationModal${location.id}`,
+                "data-bs-keyboard": "false",
+                "tabindex": "-1",
+                "aria-labelledby": "locationModal",
+                "aria-hidden": "true"
+            });
+        const modalDialogDiv = document.createElement('div');
+        modalDialogDiv.classList.add("modal-dialog", "modal-dialog-centered", "modal-dialog-scrollable");
 
-        modalDiv.appendChild(modal)
+        const modalContentDiv = document.createElement('div');
+        modalContentDiv.classList.add("modal-content");
+
+        const modalHeaderDiv = document.createElement('div');
+        modalHeaderDiv.classList.add("modal-header");
+
+        const modalTitleH5 = document.createElement('h5');
+        modalTitleH5.classList.add("modal-title");
+        modalTitleH5.innerHTML = location.name;
+
+        const modalCloseButtonElement = document.createElement('button');
+        modalCloseButtonElement.classList.add("btn-close");
+        const modalCloseButton = setElementAttributes(modalCloseButtonElement,
+            {
+                "type": "button",
+                "data-bs-dismiss": "modal",
+                "aria-label": "close"
+            });
+
+        const modalBodyDiv = document.createElement('div');
+        modalBodyDiv.classList.add("modal-body");
+
+
+        modalHeaderDiv.append(modalTitleH5, modalCloseButton);
+        modalContentDiv.append(modalHeaderDiv, modalBodyDiv);
+        modalDialogDiv.appendChild(modalContentDiv);
+        modalContainer.appendChild(modalDialogDiv);
+        modalDiv.appendChild(modalContainer);
+
+        console.log(modalDiv)
+
         columnSmall.appendChild(card);
         cardRow.appendChild(columnSmall);
     }
@@ -91,6 +116,13 @@ async function ResidentsToCharacterObjects(locationUrl, id) {
     const flatCharacterArray = characterArray.flat(1);
     const characterObjects = await FetchRickAndMortyData(`https://rickandmortyapi.com/api/character/${flatCharacterArray}`);
     return characterObjects;
+}
+
+function setElementAttributes(element, attributes) {
+    for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+    return element;
 }
 
 LocationCardConstructor();
