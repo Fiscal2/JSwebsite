0a9edd9411b0b00fe33f5d9a3f8f987c46b86658
Fiscal2 <zacharyross3@gmail.com>
2022-06-15 19:39:57 -0400

    deleted light/dark theme button

--- javascript/app.js
+++ javascript/app.js
@@ -11,22 +11,6 @@ async function FetchRickAndMortyData(url) {
 }
 
 
-function switchTheme() {
-    const currentBackground = document.body.classList[0]
-    const themeButton = document.getElementById("themebutton")
-
-    if (currentBackground == "otherBackground") {
-        themeButton.innerHTML = "Light"
-        document.body.classList.replace(currentBackground, "backgroundImage")
-        themeButton.classList.replace("btn-dark", "btn-warning")
-
-    } else {
-        themeButton.innerHTML = "Dark"
-        document.body.classList.replace(currentBackground, "otherBackground")
-        themeButton.classList.replace("btn-warning", "btn-dark")
-    }
-}
-
 
 async function dynamicCarouselConstructor() {
 
