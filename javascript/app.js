7f0db027b671d65ffac405cf0f1a34cbafdc9026
Fiscal2 <zacharyross3@gmail.com>
2022-06-16 13:09:07 -0400

    small fix

--- javascript/app.js
+++ javascript/app.js
@@ -180,7 +180,9 @@ function PaginationButtonGroupBuilder(buttonGroup = 0) {
 
 function pageButtonGroupSwapper() {
     if (currentPage > 0 && currentPage % 5 == 0) {
+        paginationList.replaceChildren();
         PaginationButtonGroupBuilder(currentPage / 5);
+        
     }
 }
 
