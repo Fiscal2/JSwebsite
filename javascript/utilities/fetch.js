6ae6eea5ab249de49fd78e0bdd320de723c80ca1
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-06-17 17:09:34 -0400

    preparing for modules

---
+++ javascript/utilities/fetch.js
@@ -0,0 +1,29 @@
+'use strict';
+// Not able to be used yet..
+const fetchRickAndMortyData = async (url) => {
+    const responseData = await fetch(url);
+
+    if (responseData.ok) {
+        return await responseData.json();
+    } else {
+        return alert(`HTTP-Error: ${responseData.status}`);
+    }
+}
+
+const paginatedFetchEndpoint = async ({ endpoint, pageCount }) => {
+    const responseArray = [];
+    let baseUrl = `https://rickandmortyapi.com/api/${endpoint}/`;
+
+    for (let i = 1; i <= pageCount; i++) {
+        const pageData = await fetchRickAndMortyData(baseUrl);
+        responseArray.push(pageData.results);
+        baseUrl = pageData.info.next;
+    }
+
+    return responseArray.flat(1);
+}
+
+export {
+    fetchRickAndMortyData,
+    paginatedFetchEndpoint
+}
