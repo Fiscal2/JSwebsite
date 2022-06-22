4fdb2f867611c7c7bda6ccee9285abf1b1abefd4
Fiscal2 <zacharyross3@gmail.com>
2022-06-22 11:14:19 -0400

    accordionBody innerHTML

--- javascript/creators.js
+++ javascript/creators.js
@@ -87,11 +87,12 @@ function accordionConstructor() {
         accordionBodyTemplate.innerHTMl =
             `
             <div class="accordion-item">
-                <h2 class="accordion-header" id=#heading${key}>
+                <h2 class="accordion-header" id="#heading${key}">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${key}" aria-expanded="false" aria-controls="collapse${key}">
                     ${value}</button>
                 </h2>
-                <div id=collapse${key} class="accordion-collapse collapse" aria-labelledby="heading${key}" data-bs-parent="#accordionParent">
+                <div id="collapse${key}" class="accordion-collapse collapse" aria-labelledby="heading${key}" data-bs-parent="#accordionParent">
+                    <div id="accordionBodyText" class="accordion-body"></div>
                 </div>
             </div>
             `.trim()
